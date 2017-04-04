90645f688d8c4b2ff41cf76c8b29a56613db4d57
RedNesto <RedNestoMC@gmail.com>
2017-04-04 22:36:21 +0200

    Refactor
    - Renamed main class
    - Changed messages handling
    - improved ping command logic
    - added color code support
    
    Signed-off-by: RedNesto <RedNestoMC@gmail.com>

---
+++ src/main/java/com/elikill58/ping/Messages.java
@@ -0,0 +1,33 @@
+package com.elikill58.ping;
+
+import org.bukkit.configuration.InvalidConfigurationException;
+import org.bukkit.configuration.file.YamlConfiguration;
+
+import java.io.IOException;
+
+public class Messages {
+
+    public static String PING_GET = Utils.applyColorCodes("&eYou have a ping of <ping> ms");
+    public static String PING_GET_OTHER = Utils.applyColorCodes("&e<name> has <ping> ms of ping");
+    public static String NOT_VALID_PLAYER = Utils.applyColorCodes("&c<arg> is not a valid player !");
+    public static String FROM_CONSOLE = Utils.applyColorCodes("&cA console has no ping !");
+    public static String RELOAD_SUCCESS = Utils.applyColorCodes("&aThe plugin has been successfully reloaded");
+    public static String RELOAD_FAIL = Utils.applyColorCodes("&cThe plugin hasn't been successfully reloaded");
+
+    public static void init() {
+        YamlConfiguration langConf = new YamlConfiguration();
+        try {
+            langConf.load(EliPing.class.getResourceAsStream("/lang/" + EliPing.LANGUAGE + ".yml"));
+
+            PING_GET = Utils.applyColorCodes(langConf.getString("ping_get"));
+            PING_GET_OTHER = Utils.applyColorCodes(langConf.getString("ping_get_other"));
+            NOT_VALID_PLAYER = Utils.applyColorCodes(langConf.getString("not_valid_player"));
+            FROM_CONSOLE = Utils.applyColorCodes(langConf.getString("from_console"));
+            RELOAD_SUCCESS = Utils.applyColorCodes(langConf.getString("reload_success"));
+            RELOAD_FAIL = Utils.applyColorCodes(langConf.getString("reload_fail"));
+        } catch(IOException | InvalidConfigurationException e) {
+            e.printStackTrace();
+        }
+    }
+
+}
--- src/main/java/com/elikill58/ping/PingCommand.java
+++ src/main/java/com/elikill58/ping/PingCommand.java
@@ -1,51 +1,54 @@
 package com.elikill58.ping;
 
 import org.bukkit.Bukkit;
-import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
-import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.command.TabExecutor;
 import org.bukkit.entity.Player;
 
-public class PingCommand implements CommandExecutor {
-	
-	@Override
-	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] arg) {
-		
-		if (sender instanceof Player) {
-			Player p = (Player) sender;
-			int ping = Main.getPing(p);
-			if (arg.length == 0) {
-				p.sendMessage(Main.ping_get.replaceAll("<ping>", ping + ""));
-			} else {
-				if(arg[0].equalsIgnoreCase("reload")){
-					Main.getInstance().reloadConfig();
-					Bukkit.getPluginManager().disablePlugin(Main.getInstance());
-					Bukkit.getPluginManager().enablePlugin(Main.getInstance());
-					p.sendMessage(Main.reload);
-				}else if (Utils.isConnectedPlayer(arg[0])) {
-					Player cible = Bukkit.getPlayer(arg[0]);
-					int otherping = Main.getPing(cible);
-					p.sendMessage(Main.ping_get_other.replaceAll("<name>", cible.getName()).replaceAll("<ping>", otherping + ""));
-				} else
-					p.sendMessage(Main.not_valid_player.replaceAll("<arg>", arg[0]));
-			}
-		} else {
-			if (arg.length == 0) {
-				sender.sendMessage("Vous ne pouvez pas savoir votre ping.");
-				sender.sendMessage(ChatColor.RED + "Usage : /ping [player]");
-			} else {
-				if (Utils.isConnectedPlayer(arg[0])) {
-					Player cible = Bukkit.getPlayer(arg[0]);
-					int otherping = Main.getPing(cible);
-					sender.sendMessage(Main.ping_get_other.replaceAll("<name>", cible.getName()).replaceAll("<ping>", otherping + ""));
-				} else
-					sender.sendMessage(Main.not_valid_player.replaceAll("<arg>", arg[0]));
-				
-			}
+import java.util.ArrayList;
+import java.util.List;
 
-		}
-		return true;
-	}
+public class PingCommand implements TabExecutor {
 
+    @Override
+    public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+
+        if (args.length == 0) {
+            if(sender instanceof Player) {
+                sender.sendMessage(Messages.PING_GET.replaceAll("<ping>", String.valueOf(EliPing.getPing((Player) sender))));
+            } else {
+                sender.sendMessage(Messages.FROM_CONSOLE);
+            }
+        } else {
+            if(args[0].equalsIgnoreCase("reload")) {
+                try {
+                    EliPing.reload();
+                    sender.sendMessage(Messages.RELOAD_SUCCESS);
+                } catch(Throwable t) {
+                    t.printStackTrace();
+                    sender.sendMessage(Messages.RELOAD_FAIL);
+                }
+            } else if (Utils.isConnectedPlayer(args[0])) {
+                Player target = Bukkit.getPlayer(args[0]);
+                int otherping = EliPing.getPing(target);
+                sender.sendMessage(Messages.PING_GET_OTHER
+                        .replaceAll("<name>", target.getName())
+                        .replaceAll("<ping>", otherping + ""));
+            } else {
+                sender.sendMessage(Messages.NOT_VALID_PLAYER
+                        .replaceAll("<arg>", args[0]));
+            }
+        }
+
+        return true;
+    }
+
+    @Override
+    public List<String> onTabComplete(CommandSender sender, Command command, String s, String[] args) {
+        List<String> list = new ArrayList<>();
+        Bukkit.getOnlinePlayers().forEach(player -> list.add(player.getName()));
+        list.add("reload");
+        return list;
+    }
 }
